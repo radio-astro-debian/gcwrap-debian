e6533e1456961ec58f6811a231bb3c98847179a2
Gijs Molenaar <gijs@pythonic.nl>
2014-03-06 17:11:23 +0200

    stuff is compiling and packaged

---
+++ casadef.py
@@ -0,0 +1,8 @@
+import os
+
+task_directory = "/usr/lib/python2.7/dist-packages/casa"
+python_library_directory = "/usr/lib/python2.7/dist-packages/casa"
+casa_version = "1.4.2"
+build_time = "Thu 2014/03/06 13:10:49 UTC"
+subversion_revision = "28496"
+subversion_url = ""
